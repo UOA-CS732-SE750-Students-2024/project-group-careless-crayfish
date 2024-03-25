5152ec3aa6860a6c1ef9987c428c27298aede9a7
Mark-VUW <zdy120939259@outlook.com>
2024-03-26 03:53:27 +1300

    set up initial proj with automated tests and github actions

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./login";
+import Dashboard from "./dashboard";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
