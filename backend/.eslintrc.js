77b8347d5bbac87e5a6b05131f8e170952b68eb0
Mark-VUW <zdy120939259@outlook.com>
2024-04-01 16:25:43 +1300

    fix eslint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,26 +1,30 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true
+  },
+  extends: "eslint:recommended",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "extends": "eslint:recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
+    {
+      files: ["./tests/*.spec.js"],
+      env: {
+        jest: true,
+      },
     },
-    "rules": {
-    }
-}
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
--- frontend/src/dashboard.jsx
+++ frontend/src/dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const CRUDPage = () => {
   const [data, setData] = useState([
