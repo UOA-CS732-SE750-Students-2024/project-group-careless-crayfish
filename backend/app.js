bdc2b53101b8eac8b3d5b79aa31d912167bea420
Mark-VUW <zdy120939259@outlook.com>
2024-04-01 16:05:27 +1300

    1

--- backend/app.js
+++ backend/app.js
@@ -9,15 +9,10 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/", indexRouter);
 app.use("/api/users", usersRouter);
