daec2d99c9ee9fdca228a0e5a894e9096c14a919
hongjian <greathongjian@yahoo.com>
2024-04-09 23:24:02 +1200

    CS732-8 add checkbox before restaurant details

--- frontend/src/components/Recommendation/RestaurantElement.jsx
+++ frontend/src/components/Recommendation/RestaurantElement.jsx
@@ -1,38 +1,59 @@
 import React from 'react';
-import Container from '@mui/material/Container';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemButton from '@mui/material/ListItemButton';
 import Collapse from '@mui/material/Collapse';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import ExpandLess from '@mui/icons-material/ExpandLess';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Checkbox from '@mui/material/Checkbox';
 
 const RestaurantElement = ({ restaurant }) => {
+
   const [open, setOpen] = React.useState(false);
 
   const handleClick = (event) => {
     setOpen(!open);
   };
 
+  console.log(restaurant);
+
   return (<>
-      <ListItemButton key={restaurant.name}
+      {/* Restaurant element */}
+      <ListItemButton key={restaurant.index}
         selected = {restaurant.selected === restaurant.index}
         onClick={(event) => {
-          restaurant.onClick(event, restaurant.index);
+          restaurant.handleListItemClick(event, restaurant.index);
           handleClick(event);
         }}
       >
+        {/* Checkbox to select the restaurant */}
+        <ListItemIcon onClick={restaurant.handleToggle(restaurant.index)}>
+          <Checkbox
+            edge="start"
+            checked={restaurant.checked.indexOf(restaurant.index) !== -1}
+            tabIndex={-1}
+            disableRipple
+            inputProps={{ 'aria-labelledby': restaurant.labelId }}
+          />
+        </ListItemIcon>
+
+        {/* Restaurant details */}
         <ListItemText
           primary={`${restaurant.name} (${restaurant.priceRange})`}
           secondary={`Description: ${restaurant.description} Location: ${restaurant.location}`}
         />
-        {/* Add a container for the image, map, and other details */}
+
+        {/* Expand button */}
         {open ? <ExpandLess /> : <ExpandMore />}
       </ListItemButton>
+
+      {/* Collapsed details */}
       <Collapse in={open} timeout="auto" unmountOnExit>
         {`${restaurant.name} details`}
         <br/>
         {`place holder for image, map, and other details`}
       </Collapse>
+
     </>
   );
 };
--- frontend/src/components/Recommendation/RestaurantRecommendations.jsx
+++ frontend/src/components/Recommendation/RestaurantRecommendations.jsx
@@ -8,9 +8,20 @@ import Divider from '@mui/material/Divider';
 import RestaurantElement from './RestaurantElement';
 
 export const RestaurantRecommendations = () => {
+  // recommendations is an array of objects, each object represents a restaurant
   const [recommendations, setRecommendations] = useState([]);
+
+  // selectedIndex is the index of the selected restaurant to display details
   const [selectedIndex, setSelectedIndex] = React.useState(0);
 
+  /**
+   * Handles the click event of a restaurant.
+   * @param {React.MouseEvent<HTMLDivElement, MouseEvent>} event - The click event.
+   * 
+   * @param {number} index - The index of the selected restaurant.
+   * 
+   * @returns {void}
+   */
   const handleListItemClick = (
     event, // React.MouseEvent<HTMLDivElement, MouseEvent>
     index, // number
@@ -18,7 +29,29 @@ export const RestaurantRecommendations = () => {
     setSelectedIndex(index);
   };
 
+  // checked is the index of the selected restaurant for further usage
+  const [checked, setChecked] = React.useState([]);
+
+  /**
+   * Handles the selection of a restaurant.
+   *
+   * @param {number} value - The index of the selected restaurant.
+   */
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
   useEffect(() => {
+    // Fetch recommendations from the API
     const fetchRecommendations = async () => {
       try {
         console.log("Fetching recommendations");
@@ -40,9 +73,14 @@ export const RestaurantRecommendations = () => {
       </Typography>
       <List>
         {recommendations.map((restaurant, index) => (
-          restaurant.onClick = handleListItemClick,
-          restaurant.selected = selectedIndex,
           restaurant.index = index,
+
+          restaurant.handleListItemClick = handleListItemClick,
+          restaurant.selected = selectedIndex,
+          
+          restaurant.checked = checked,
+          restaurant.handleToggle = handleToggle,
+          restaurant.lableId = `checkbox-list-label-${restaurant.index}`,
           <RestaurantElement key={restaurant.name} restaurant={restaurant} />
         ))}
         <Divider variant="inset" component="li" />
