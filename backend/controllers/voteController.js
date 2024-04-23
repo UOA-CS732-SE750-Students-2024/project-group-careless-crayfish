c58d12689046adb6ed159aa3173bed190251104d
hongjian <greathongjian@yahoo.com>
2024-04-23 19:59:36 +1200

    CS732-8 add swagger js doc desc

--- backend/controllers/voteController.js
+++ backend/controllers/voteController.js
@@ -1,8 +1,42 @@
+/**
+ * Express router for handling vote-related routes.
+ * @module voteController
+ */
+
 const express = require("express");
 const router = express.Router();
 const logger = require('../utils/logger.js');
 const voteResult = require("../../wiremock/__files/auckland_restaurants.json")
 
+/**
+ * Route for creating a new vote.
+ * @name POST /restaurant
+ * @function
+ * @async
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * 
+ * @swagger
+ * /restaurant:
+ *   post:
+ *     tags:
+ *     - Vote Controller
+ *     summary: Create a new vote.
+ *     consumes: application/json
+ *     parameters:
+ *       - in: body
+ *         name: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             restaurantId:
+ *               type: string
+*      responses:
+ *       '200':
+ *         description: Vote created successfully
+ *       '500':
+ *         description: Internal server error
+ */
 router.post("/restaurant", async function createVote(req, res) {
   try {
     // dummy code
@@ -15,6 +49,35 @@ router.post("/restaurant", async function createVote(req, res) {
   }
 });
 
+/**
+ * Route for fetching a vote by ID.
+ * @name GET /restaurant/:voteId
+ * @function
+ * @async
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ * 
+ * @swagger
+ * /restaurant/{voteId}:
+ *   get:
+ *     tags:
+ *     - Vote Controller
+ *     summary: Get vote by voteId
+ *     parameters:
+ *       - in: path
+ *       name: voteId
+ *       schema:
+ *         type: string
+ *         required: true
+ *         description: Vote ID
+ *     responses:
+ *       '200':
+ *         description: Vote found
+ *       '404':
+ *         description: Vote not found
+ *       '500':
+ *         description: Internal server error
+ */
 router.get("/restaurant/:voteId", async (req, res) => {
   try {
     // dummy code
@@ -34,4 +97,5 @@ router.get("/restaurant/:voteId", async (req, res) => {
 });
 
 // Add other routes as needed
+
 module.exports = router;
